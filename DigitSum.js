07b26b567175a239c20e9fb23316b1491c3784ef
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-07 10:48:01 +0600

    For loop Multiple Digit Sum
    for loop Multiple input Digit Sum in JavaScript

---
+++ DigitSum.js
@@ -0,0 +1,11 @@
+let numbers = prompt("Enter Multiple numbers: ");
+numbers = numbers;
+let sum = 0;
+for (let i = 0; i < numbers.length; i++){
+  sum += parseInt(numbers[i]);
+}
+document.write("The sum of the NUmber is: " + sum);
+
+
+
+// for lopp Multiple input Digit Sum in JavaScript
